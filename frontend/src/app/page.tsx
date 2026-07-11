1bfc3ad2cf9df3a531ebe010a70ed8580bba4e21
MD-ALL-SHAHRIA <mdallshahria@gmail.com>
2026-07-11 19:35:08 +0600

    update login page to use new auth layout

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,39 @@
-import LoginWrapper from "@/components/login/LoginWrapper";
+import AuthLayout from "@/components/auth/AuthLayout";
+import LoginForm from "@/components/login/LoginForm";
 
 export default function Home() 
+
 {
-    return <LoginWrapper />;
+  return (
+    <AuthLayout
+      heroImage={{
+        lightImageSrc: "/assets/images/login.png",
+        darkImageSrc: "/assets/images/login1.png",
+        width: 1269,
+        height: 1240,
+        imageClassName: "max-w-full lg:max-w-[633px] w-auto h-auto",
+      }}
+
+
+      header={{
+        subtitle: "Welcome back",
+        title: "Login to your account",
+      }}
+
+      googleButton={{
+        text: "Or sign-in with google",
+      }}
+
+      footer={{
+        text: "Don't have an account?",
+        linkText: "Create New Account",
+        href: "/registration",
+      }}
+
+    >
+      <LoginForm/>
+      </AuthLayout>
+
+
+  );
 }
