cf70d937366db53caf98b3c9730f8534407d965f
MD-ALL-SHAHRIA <mdallshahria@gmail.com>
2026-07-11 19:42:44 +0600

    add registration page route

---
+++ frontend/src/app/registration/page.tsx
@@ -0,0 +1,35 @@
+import AuthLayout from "@/components/auth/AuthLayout";
+import RegistrationForm from "@/components/registration/RegistrationForm";
+
+export default function RegistrationPage() 
+
+
+{
+  return (
+    <AuthLayout
+      heroImage={{
+        lightImageSrc: "/assets/images/registration.png",
+        darkImageSrc: "/assets/images/registration1.png",
+        width: 1928,
+        height: 1422,
+      }}
+      header={{
+        subtitle: "Get Started Now",
+        title: "Registration",
+      }}
+
+      googleButton={{
+        text: "Register with google",
+      }}
+
+      footer={{
+        text: "Already have an account?",
+        linkText: "Login",
+        href: "/",
+      }}
+
+    >
+      <RegistrationForm />
+      </AuthLayout>
+  );
+}
