d17c5e74946623c0368b7e016c6b5252b689ec66
MD-ALL-SHAHRIA <mdallshahria@gmail.com>
2026-07-13 00:50:58 +0600

    update auth ui components

--- frontend/src/components/auth/AuthFooter.tsx
+++ frontend/src/components/auth/AuthFooter.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 
 interface AuthFooterProps 
+
+
 {
   text: string;
   linkText: string;
@@ -8,20 +10,19 @@ interface AuthFooterProps
 }
 
 export default function AuthFooter({ text, linkText, href }: AuthFooterProps) 
+
+
 {
   return (
     <div className="flex flex-wrap -mx-[15px]">
-
       <div className="w-full px-[15px]">
         <div className="text-center">
-
           <p className="text-[14px]">
-
-            {text} <Link href={href} className="text-[#1890FF] hover:underline">{linkText}</Link>
-
+            {text}{" "}
+            <Link href={href} className="text-[#1890FF] hover:underline">
+              {linkText}
+            </Link>
           </p>
-
-          
         </div>
       </div>
     </div>
